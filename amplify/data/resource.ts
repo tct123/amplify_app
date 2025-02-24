de9f53845dab7372fc4f0b47fea9da203790150c
kallehallden <halldenkalle@gmail.com>
2025-02-24 12:19:36 +0100

    added models

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,50 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Hello: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
+  User: a.model({
+    userId: a.id().required(),
+    name: a.string().required(),
+    age: a.integer(),
+    gender: a.string(),
+    gender_preference: a.string(),
+    age_preference: a.integer().array(),
+    location: a.customType({
+      lat: a.float().required(),
+      long: a.float().required(),
+    }),
+    radius: a.integer(),
+    pictures: a.string().array(),
+    profile_picture: a.string(),
+    // Inverse relationships: reference the join model field names.
+    likes: a.hasMany('UserLikes', 'id'),
+    likedBy: a.belongsTo('UserLikes', 'userId'),
+    matches: a.hasMany('UserMatches', 'id'),
+    matchedBy: a.belongsTo('UserMatches', 'userId'),
+    dislikes: a.hasMany('UserDislikes', 'id'),
+    dislikedBy: a.belongsTo('UserDislikes', 'userId'),
+  })
+  .identifier(['userId'])
+  .authorization(allow => [allow.publicApiKey()]),
+
+  UserLikes: a.model({
+    id: a.id().required(),
+    // The user doing the like
+    user: a.belongsTo('User', 'id'),
+    // The user being liked
+    likedUsers: a.hasMany('User', 'userId'),
+  }).authorization(allow => [allow.publicApiKey()]),
+
+  UserMatches: a.model({
+    id: a.id().required(),
+    user: a.belongsTo('User', 'id'),
+    userMatches: a.hasMany('User', 'userId'),
+  }).authorization(allow => [allow.publicApiKey()]),
+
+  UserDislikes: a.model({
+    id: a.id().required(),
+    user: a.belongsTo('User', 'id'),
+    dislikedUsers: a.hasMany('User', 'userId'),
+  }).authorization(allow => [allow.publicApiKey()])
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,35 +52,8 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
-  },
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 }
+  }
 });
 
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
