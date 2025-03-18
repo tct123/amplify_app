fc03843dda77c549afb5575833aa4d3962500276
kallehallden <halldenkalle@gmail.com>
2025-03-18 11:34:32 +0100

    added eventfucntionhandler to backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,12 +2,14 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { filterUsersFunctionHandler } from './functions/filter-users/resource';
+import { createEventFunctionHandler } from './functions/create-event/resource'; 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
   auth,
   data,
-  filterUsersFunctionHandler
+  filterUsersFunctionHandler,
+  createEventFunctionHandler
 });
 
