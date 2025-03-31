092cb2875ad7a7f0522d844d68414352f27ff609
kallehallden <halldenkalle@gmail.com>
2025-03-31 11:05:43 +0200

    added profile picture storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,12 +3,14 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { filterUsersFunctionHandler } from './functions/filter-users/resource';
 import { createEventFunctionHandler } from './functions/create-event/resource'; 
+import { storage } from './storage/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
   auth,
   data,
+  storage,
   filterUsersFunctionHandler,
   createEventFunctionHandler
 });
