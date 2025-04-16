f46dc4a7a3fb3f8569cc127311b0bdb79523a0f1
kallehallden <halldenkalle@gmail.com>
2025-04-16 10:00:26 +0200

    tried adding policy to lambda

--- amplify/functions/match-users/resource.ts
+++ amplify/functions/match-users/resource.ts
@@ -3,7 +3,8 @@ import * as path from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineFunction } from "@aws-amplify/backend";
 import { DockerImage, Duration } from "aws-cdk-lib";
-import { Code, Function, Runtime, PolicyStatement } from "aws-cdk-lib/aws-lambda";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam"; // Correct import
 
 const functionDir = path.dirname(fileURLToPath(import.meta.url));
 
